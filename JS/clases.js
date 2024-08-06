bd3c26a71c681d3f5d021ca781bfa2048c20d85f
Gabriel Rouillet <rouilletgabriel@gmail.com>
2024-08-06 20:21:29 -0300

    proyecto final1

--- JS/clases.js
+++ JS/clases.js
@@ -8,10 +8,11 @@ class Cliente {
 }
 
 class Producto {
-    constructor(id, nombre, precio) {
+    constructor(id, nombre, precio,img) {
         this.id = id;
         this.nombre = nombre;
         this.precio =precio;
+        this.img=img;
     }
 }
 
